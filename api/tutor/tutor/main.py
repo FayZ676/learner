c4164fbcb903daa776445300ab5dea368f14c372
FayZ676 <faizififita1@gmail.com>
2025-03-19 21:51:00 -0700

    Update subject.

--- api/tutor/tutor/main.py
+++ api/tutor/tutor/main.py
@@ -12,8 +12,9 @@ app = FastAPI()
 def lesson():
     db = DB()
     date = datetime.now().strftime("%Y-%m-%d")
+    subject = "Python data structures and algorithms. Particularly for interview style and leetcode style questions."
     if lesson := db.get_lesson_by_date(date):
         return lesson
-    lesson = get_lesson("Python data structures", date)
+    lesson = get_lesson(subject, date)
     db.save_lesson(lesson)
     return lesson

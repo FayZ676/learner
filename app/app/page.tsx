72adb52ab459a6a47e249a40feac170ee4e74618
FayZ676 <faizififita1@gmail.com>
2025-03-20 20:46:09 -0700

    Get clients local date

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -3,16 +3,25 @@
 import { useEffect, useState } from "react";
 
 import getLesson from "./actions/getLesson";
+
 import LessonView from "./components/LessonView";
 import { Lesson } from "./types";
+
+
+function getLocalDate(): string {
+  const today = new Date();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+  return `${today.getFullYear()}-${month}-${day}`;
+}
+
 
 export default function Home() {
   const [lesson, setLesson] = useState<Lesson | null>(null)
 
   useEffect(() => {
     const fetchLesson = async () => {
-      const dateString = new Date().toISOString().slice(0, 10)
-      setLesson(await getLesson(dateString))
+      setLesson(await getLesson(getLocalDate()))
     };
     fetchLesson();
   }, [])

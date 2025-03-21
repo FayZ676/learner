5d6014fa68bc04be3debfc9944fde585344ec637
FayZ676 <faizififita1@gmail.com>
2025-03-21 15:56:22 -0700

    Update view.

--- app/app/components/SubjectsView.tsx
+++ app/app/components/SubjectsView.tsx
@@ -44,38 +44,49 @@ export default function SubjectsView({ updateActiveSubject, activeSubject, loadi
     }
 
     return (
-        <div className='flex gap-2 ml-auto '>
-            {
-                subjects && <select className="select" value={activeSubject} onChange={handleChangeSubject}>
-                    {subjects.map((subject, index) => {
-                        return (<option key={index}>{subject}</option>)
-                    })}
-                </select>
-            }
-            <button className="btn" disabled={loadingLesson} onClick={() => {
-                const modal = document.getElementById('my_modal_1') as HTMLDialogElement;
-                if (modal) {
-                    modal.showModal();
+        <>
+            <h2>Subject</h2>
+            <div className='flex gap-2 ml-auto '>
+                {
+                    subjects && <select className="select w-full" value={activeSubject} onChange={handleChangeSubject}>
+                        {subjects.map((subject, index) => {
+                            return (<option key={index}>{subject}</option>)
+                        })}
+                    </select>
                 }
-            }}>new subject</button>
-            <dialog id="my_modal_1" className="modal">
-                <div className="modal-box">
-                    <textarea className="textarea w-full" placeholder="Subject" value={subjectInput || ""} onChange={(e) => { setSubjectInput(e.target.value) }}></textarea>
-                    <div className="modal-action">
-                        <form method="dialog" onSubmit={(e) => {
-                            e.preventDefault();
-                            handleAddSubject();
-                        }}>
-                            <button className="btn" disabled={!subjectInput} onClick={() => {
-                                const modal = document.getElementById('my_modal_1') as HTMLDialogElement;
-                                if (modal) {
-                                    modal.close();
-                                }
-                            }}>add</button>
-                        </form>
+                <button className="btn" disabled={loadingLesson} onClick={() => {
+                    const modal = document.getElementById('my_modal_1') as HTMLDialogElement;
+                    if (modal) {
+                        modal.showModal();
+                    }
+                }}>manage</button>
+                <dialog id="my_modal_1" className="modal">
+                    <div className="modal-box">
+                        <ul className="list">
+                            {subjects?.map((subject, index) => {
+                                return (<li className="list-row flex justify-between">
+                                    <span>{subject}</span>
+                                    <button className="btn">delete</button>
+                                </li>)
+                            })}
+                        </ul>
+                        <textarea className="textarea w-full" placeholder="Subject" value={subjectInput || ""} onChange={(e) => { setSubjectInput(e.target.value) }}></textarea>
+                        <div className="modal-action">
+                            <form method="dialog" onSubmit={(e) => {
+                                e.preventDefault();
+                                handleAddSubject();
+                            }}>
+                                <button className="btn" disabled={!subjectInput} onClick={() => {
+                                    const modal = document.getElementById('my_modal_1') as HTMLDialogElement;
+                                    if (modal) {
+                                        modal.close();
+                                    }
+                                }}>add</button>
+                            </form>
+                        </div>
                     </div>
-                </div>
-            </dialog>
-        </div>
+                </dialog>
+            </div>
+        </>
     )
 }
